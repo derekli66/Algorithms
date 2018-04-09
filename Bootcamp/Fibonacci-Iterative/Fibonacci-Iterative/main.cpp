691591c841b1cfb09ce0724b599deb267b43b895
Derek Lee <derek.lee@positivegrid.com>
2018-04-09 23:19:43 +0800

    Update Fibonacci iterative solution. The new solution was completed by while loop and without any container

--- Bootcamp/Fibonacci-Iterative/Fibonacci-Iterative/main.cpp
+++ Bootcamp/Fibonacci-Iterative/Fibonacci-Iterative/main.cpp
@@ -11,6 +11,24 @@
 
 using namespace std;
 
+unsigned int FibonacciByLoop(unsigned int n) {
+    if (n < 2) return 1;
+    
+    unsigned int a = 0;
+    unsigned int b = 1;
+    unsigned int result = 0;
+    int idx = 2;
+    
+    while (idx <= n) {
+        result = a + b;
+        a = b;
+        b = result;
+        idx++;
+    }
+    
+    return result;
+}
+
 unsigned int FibonacciIterative(unsigned int n) {
     vector<unsigned int> results{0, 1};
     
@@ -25,10 +43,12 @@ unsigned int FibonacciIterative(unsigned int n) {
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    unsigned int input = 15;
+    unsigned int input = 20;
     unsigned int result = FibonacciIterative(input);
+    unsigned int result2 = FibonacciByLoop(input);
     
-    cout << "Fibonacci result for input, " << input << ", " << "is " << result << endl;
-    
+    cout << "FibonacciIterative result for input, " << input << ", " << "is " << result << endl;
+    cout << "FibonacciByLoop result for input, " << input << ", " << "is " << result2 << endl;
+
     return 0;
 }
